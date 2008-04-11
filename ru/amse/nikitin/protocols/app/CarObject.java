de85f0c3b8f8f0ecbb15829aa759363d8c8ff34a
pavel.a.nikitin <pavel.a.nikitin@7a4cfcf8-6a46-0410-999c-1b069df95065>
2008-04-11 16:22:20 +0000

    Random waypoint model implemented.

--- ru/amse/nikitin/protocols/app/CarObject.java
+++ ru/amse/nikitin/protocols/app/CarObject.java
@@ -1,18 +1,38 @@
 package ru.amse.nikitin.protocols.app;
 
+import java.util.Random;
+
 import ru.amse.nikitin.sensnet.impl.MonitoredPacket;
 import ru.amse.nikitin.sensnet.impl.MonitoredObject;
 import ru.amse.nikitin.sensnet.impl.MonitoredObjectModule;
 import ru.amse.nikitin.simulator.impl.Time;
 
 public class CarObject extends MonitoredObjectModule {
+	protected static final int maxX = 600;
+	protected static final int maxY = 800;
+	protected static final int speed = 10;
+	protected static final Random randomizer = new Random();
+	
 	protected int helloCount = 1;
+	protected int stepsMax;
+	protected int stepsTotal;
+	protected int oldX;
+	protected int oldY;
+	protected int newX;
+	protected int newY;
+	protected int offsetX;
+	protected int offsetY;
 	protected final static Time oneUnitTime = new Time(0);
 	
 	Runnable r = new Runnable() {
 		public void run() {
-			sensing.setX(sensing.getX() + 10);
-			sensing.setY(sensing.getY() + 5);
+			if (stepsTotal <= stepsMax) {
+				stepsTotal++;
+				sensing.setX(oldX + (offsetX * stepsTotal) / stepsMax);
+				sensing.setY(oldY + (offsetY * stepsTotal) / stepsMax);
+			} else {
+				newWaypoint();
+			}
 			// System.out.println("SETTING");
 			scheduleEvent(this, oneUnitTime); // resend
 			MonitoredPacket p = new MonitoredPacket(sensing, 1);
@@ -21,6 +41,20 @@ public class CarObject extends MonitoredObjectModule {
 			sendMsgToLower(p);
 		}
 	};
+	
+	protected void newWaypoint() {
+		oldX = newX;
+		oldY = newY;
+		newX = randomizer.nextInt(maxX);
+		newY = randomizer.nextInt(maxY);
+		int dx = newX - oldX;
+		int dy = newY - oldY;
+		int len = (int)Math.sqrt(dx*dx + dy*dy);
+		stepsMax = len / speed;
+		stepsTotal = 0;
+		offsetX = newX - oldX;
+		offsetY = newY - oldY;
+	}
 
 	public CarObject(MonitoredObject sensing) {
 		super(sensing);
@@ -31,6 +65,9 @@ public class CarObject extends MonitoredObjectModule {
 	}
 
 	public void init() {
+		newX = sensing.getX();
+		newY = sensing.getY();
+		newWaypoint();
 		scheduleEvent(r, oneUnitTime);
 	}
 	
