0f75d6e13f4262673ce8a141bd628a8d369b9e80
pavel.a.nikitin <pavel.a.nikitin@7a4cfcf8-6a46-0410-999c-1b069df95065>
2008-05-02 20:41:08 +0000

    Model Tutorial added.

--- ru/amse/nikitin/models/hugetest/Const.java
+++ ru/amse/nikitin/models/hugetest/Const.java
@@ -2,7 +2,7 @@ package ru.amse.nikitin.models.hugetest;
 
 public class Const {
 	public static final int offset = 5;
-	public static final int count = 100000;
+	public static final int count = 10000;
 	public static final int width = 10;
 	public static final String fileName = "huge.txt";
 	public static final int iterationsCount = width + offset + 2;
