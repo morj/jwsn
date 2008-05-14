6b65c0917c3ac972a5af49ef4a53dde7e6d46b84
pavel.a.nikitin <pavel.a.nikitin@7a4cfcf8-6a46-0410-999c-1b069df95065>
2008-05-14 08:15:15 +0000

    Unnecessary commented-out code removed.

--- ru/amse/nikitin/models/M4_CarsCentrAloha.java
+++ ru/amse/nikitin/models/M4_CarsCentrAloha.java
@@ -1,6 +1,7 @@
 package ru.amse.nikitin.models;
 
 import javax.swing.ImageIcon;
+
 import ru.amse.nikitin.models.aloha.GraphProduceStrategy;
 import ru.amse.nikitin.net.IGate;
 import ru.amse.nikitin.protocols.app.BsApp;
@@ -21,46 +22,6 @@ import ru.amse.nikitin.simulator.IDispatcher;
 import ru.amse.nikitin.protocols.app.CarObject;
 import ru.amse.nikitin.protocols.app.TemperatureObject;
 
-/* abstract class MotGenerator implements IMotGenerator {
-	
-	protected void connectDuplexGates(IGate a, IGate b) {
-		a.setFrom(b);
-		a.setTo(b);
-		b.setTo(a);
-		b.setFrom(a);
-	}
-	
-	protected void declareDuplexGates(MotModule a, MotModule b) {
-		IGate u = a.declareGate("upper");
-		IGate l = b.declareGate("lower");
-		connectDuplexGates(u, l);
-	}
-
-
-	protected void connectModules(Mot m, MotModule mac, MotModule net, MotModule app) {
-		m.addModule("mac", mac);
-		m.addModule("net", net);
-		m.addModule("app", app);
-		
-		IGate gate = mac.declareGate("lower");
-		
-		IGate input = m.declareInputGate(WirelessPacket.class);
-		input.setTo(gate);
-		gate.setFrom(input);
-		
-		IGate output = m.getOutputGate();
-		gate.setTo(output);
-		output.setFrom(gate);
-		
-		declareDuplexGates(mac, net);
-		declareDuplexGates(net, app);
-	}
-	
-	public Mot generateMot(int x, int y, double power, double threshold) {
-		return null;
-	}
-	
-} /**/
 
 class BsMotGenerator2 extends MotGenerator {
 	public Mot generateMot(int x, int y, double power, double threshold) {
